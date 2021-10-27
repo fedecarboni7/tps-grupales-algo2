7a2e02fd391654ad30cbca67904b58256c16a16b
aallones <allonesaldana@gmai.com>
2021-10-27 17:41:49 -0300

    Corrijo struct hash y agrego hash_cantidad

--- TDA-Hash/hash.c
+++ TDA-Hash/hash.c
@@ -5,7 +5,8 @@
 
 typedef struct hash {
     lista_t** tabla;
-    size_t largo;
+    size_t m;
+    hash_destruir_dato_t destruir_dato;
 } hash_t;
 
 // Definición del struct campo
@@ -15,3 +16,17 @@ typedef struct campo {
     void* dato;
 } campo_t;
 
+size_t hash_cantidad(const hash_t *hash) {
+    size_t cantidad = 0;
+    for(size_t i = 0; i < hash->m; i++) {
+        if(hash->lista[i] && !lista_esta_vacia(hash->lista[i])) {
+            lista_iter_t *iter = lista_iter_crear(hash->lista[i]);
+            while (lista_iter_ver_actual(iter)) {
+                lista_iter_avanzar(iter);
+                cantidad++;
+            }
+            lista_iter_destruir(iter);
+        }
+    }
+    return cantidad; 
+}
